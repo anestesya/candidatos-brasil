559f127ddfbc31a0aaa306ad31ea0c17987ac67e
Dirceu Pauka Jr <dirceuu@gmail.com>
2014-08-21 17:03:34 -0300

    add bandeiras de estados

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,33 @@
 $(document).on("pageinit", "#index", function(event) {
 
-  // preenche a lista com estados
+  var render = [];
 
-  // pega lista com estados para salvar aqui
+  for (e in Estados) {
+    var estado = Estados[e];
+    render.push(
+      "<li>",
+      "<a href='#'>",
+      "<img src='/static/img/bandeiras_estados/", estado["sigla"],".png'>",
+      "<h2>", estado["nome"], "</h2>",
+      "</a>",
+      "</li>"
+    );
+  }
+
+  var listview = $(this).find('ul[data-role=listview]');
+  listview.html(render.join(''));
+  listview.listview('refresh');
+
+  // verifica no webstorage se tiver estado salvo vai para tela de cargos
+  if (!store.enabled) {
+    alert('Local storage is not supported by your browser. Please disable "Private Mode", or upgrade to a modern browser.')
+    return;
+  }
+  var user = store.get('user');
+
+});
 
 
+$(document).on("pageinit", "#cargos", function(event) {
 
 });
